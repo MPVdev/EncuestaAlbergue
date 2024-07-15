0596247b9e99a2e1cf4742cce28789c768f295b7
P-Diaz <henry.diaz054@ist17dejulio.edu.ec>
2024-07-15 17:12:06 -0500

    Creacion del metodo JsonCreator

--- app/src/main/java/com/mpv/encuestaalbergue/MainActivity.java
+++ app/src/main/java/com/mpv/encuestaalbergue/MainActivity.java
@@ -4,12 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +35,41 @@ public class MainActivity extends AppCompatActivity {
     Button btnGuardar, btnEnviar;
     Spinner sprP5, sprP6, sprP7, sprP9, sprP11;
     CheckBox cbP4Perros, cbP4Gatos, cbP4Conejos, cbP4Pajaros, cbP8Op1, cbP8Op2, cbP8Op3, cbP12Op1, cbP12Op2, cbP12Op3, cbP12Op4, cbP12Op5, cbP12Op6, cbP12Op7;
+    String respuestaJson = "";
+    JsonCreator jsonCreator;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        conexionFrontBack();
+
+        String[] alimentacion = getResources().getStringArray(R.array.alimentacion);
+        ArrayAdapter<String> adapterAlimentacion = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, alimentacion);
+        adapterAlimentacion.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        String[] adopcionOp = getResources().getStringArray(R.array.adopcionOp);
+        ArrayAdapter<String> adapterAdopcionOp = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, adopcionOp);
+        adapterAdopcionOp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        String[] frecuencia = getResources().getStringArray(R.array.frecuencia);
+        ArrayAdapter<String> adapterFrecuencia = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, frecuencia);
+        adapterFrecuencia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        String[] op = getResources().getStringArray(R.array.Op);
+        ArrayAdapter<String> adapterOp = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, op);
+        adapterOp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        sprP5.setAdapter(adapterAlimentacion);
+        sprP6.setAdapter(adapterAdopcionOp);
+        sprP7.setAdapter(adapterFrecuencia);
+        sprP9.setAdapter(adapterOp);
+        sprP11.setAdapter(adapterOp);
+
+
+    }
+    public void conexionFrontBack(){
         tvTitulo = findViewById(R.id.tvTitulo);
         tvP1 = findViewById(R.id.tvP1);
         tvP2 = findViewById(R.id.tvP2);
@@ -65,27 +111,43 @@ public class MainActivity extends AppCompatActivity {
         cbP12Op5 = findViewById(R.id.cbP12Op5);
         cbP12Op6 = findViewById(R.id.cbP12Op6);
         cbP12Op7 = findViewById(R.id.cbP12Op7);
+    }
 
-        String[] alimentacion = getResources().getStringArray(R.array.alimentacion);
-        ArrayAdapter<String> adapterAlimentacion = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, alimentacion);
-        adapterAlimentacion.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    public void anadirRespuestas() {
+        String P1, P5, P6, P7, P9, P10, P11, P13;
+        int P2, P3;
+        ArrayList<String> P4, P8, P12;
+        P4 = new ArrayList<>();
+        P1 = edP1.getText().toString();
+        P2 = Integer.parseInt(edP2.getText().toString());
+        P3 = Integer.parseInt(edP3.getText().toString());
+    }
 
-        String[] adopcionOp = getResources().getStringArray(R.array.adopcionOp);
-        ArrayAdapter<String> adapterAdopcionOp = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, adopcionOp);
-        adapterAdopcionOp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        String[] frecuencia = getResources().getStringArray(R.array.frecuencia);
-        ArrayAdapter<String> adapterFrecuencia = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, frecuencia);
-        adapterFrecuencia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        String[] op = getResources().getStringArray(R.array.Op);
-        ArrayAdapter<String> adapterOp = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, op);
-        adapterOp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        sprP5.setAdapter(adapterAlimentacion);
-        sprP6.setAdapter(adapterAdopcionOp);
-        sprP7.setAdapter(adapterFrecuencia);
-        sprP9.setAdapter(adapterOp);
-        sprP11.setAdapter(adapterOp);
+    public void ConsumirAPI(){
+        String url="";
+        OkHttpClient cliente = new OkHttpClient();
+        Request get = new Request.Builder()
+                .url(url)
+                .build();
+        cliente.newCall(get).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+            @Override
+            public void onResponse(Call call, Response response) {
+                try {
+                    ResponseBody responseBody = response.body();
+                    if (!response.isSuccessful()) {
+                        throw new IOException("Unexpected code " + response);
+                    }else{
+                        respuestaJson=responseBody.string();
+                    }
+                    Log.i("data", responseBody.string());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
